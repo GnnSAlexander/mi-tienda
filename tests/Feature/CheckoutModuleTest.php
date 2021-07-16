289812a6901fb44b98499642b151152a4ba0e9b6
gacarrasquilla <gennys.carrasquilla@platypus-agency.com>
2021-07-16 15:47:29 -0500

    Add test of validation and order creation

---
+++ tests/Feature/CheckoutModuleTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Order;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CheckoutModuleTest extends TestCase
+{
+    /**
+     * Verify it loads the checkout page
+     *
+     * @return void
+     */
+    public function  testItLoadsTheCheckoutPage()
+    {
+
+        $this->get(route('checkout'))
+            ->assertSee('Billing Information')
+            ->assertStatus(200);
+
+    }
+
+    /**
+     * Verify The name is Required
+     *
+     * @return void
+     */
+
+    public function testTheNameIsRequired()
+    {
+        //$this->withoutExceptionHandling();
+        $data = [
+            'name' => '',
+            'email' => 'example@example.com',
+            'phone' => '12345678'
+        ];
+
+        $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data)
+            ->assertRedirect(route('checkout'))
+            ->assertSessionHasErrors(['name']);
+
+
+        $data['name'] = 'te';
+
+        $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data)
+            ->assertRedirect(route('checkout'))
+            ->assertSessionHasErrors(['name']);
+    }
+
+    /**
+     * Verify The email is Required
+     *
+     * @return void
+     */
+
+    public function testTheEmailIsRequired()
+    {
+        //$this->withoutExceptionHandling();
+        $data = [
+            'name' => 'Pepito Perez',
+            'email' => '',
+            'phone' => '12345678'
+        ];
+
+        $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data)
+            ->assertRedirect(route('checkout'))
+            ->assertSessionHasErrors(['email']);
+
+
+        $data['email'] = 'email-no-valid';
+
+        $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data)
+            ->assertRedirect(route('checkout'))
+            ->assertSessionHasErrors(['email']);
+    }
+
+    /**
+     * Verify The phone is Required
+     *
+     * @return void
+     */
+
+    public function testPhoneIsRequired()
+    {
+        //$this->withoutExceptionHandling();
+        $data = [
+            'name' => 'Pepito Perez',
+            'email' => 'example@example.com',
+            'phone' => '',
+            'total' => '175000',
+            'currency' => 'COP'
+        ];
+
+        $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data)
+            ->assertRedirect(route('checkout'))
+            ->assertSessionHasErrors(['phone']);
+
+
+        $data['phone'] = 'te';
+
+        $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data)
+            ->assertRedirect(route('checkout'))
+            ->assertSessionHasErrors(['phone']);
+    }
+
+    /**
+     * Verify the order is created
+     *
+     * @return void
+     */
+
+    public function testTheOrderIsCreated()
+    {
+        $this->withoutExceptionHandling();
+        $data = [
+            'name' => 'Pepito Perez',
+            'email' => 'example@example.com',
+            'phone' => '1234567890',
+            'total' => '175000',
+            'currency' => 'COP'
+        ];
+
+        $response =  $this->from(route('checkout'))
+            ->post(route('checkout.store'),$data);
+
+
+        $lastId = DB::getPdo()->lastInsertId();
+
+        $response->assertRedirect(route('summary',['order' => $lastId]));
+
+        $order = $this->assertDatabaseHas('orders',[
+            'customer_name' => 'Pepito Perez',
+            'customer_email' => 'example@example.com',
+            'customer_mobile' => '1234567890'
+        ]);
+
+
+
+    }
+
+}
